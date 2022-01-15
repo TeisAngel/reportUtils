e8e8faecf90a610ad004128b25d922820ed3fbcb
Nazarii <nazariibe@softwareplanet.uk.com>
2022-01-15 15:15:53 +0200

    Reformat code with prettier

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const createReport = template => {
-  if (!template) throw new Error(
-    'required argument "template" is not passed',
-  );
+const createReport = (template) => {
+  if (!template) throw new Error('required argument "template" is not passed');
 
   const cache = new Map();
 
-  const proxy = new Proxy({}, {
-    get(target, prop, receiver) {
-      if (cache.has(prop)) return cache.get(prop);
+  const proxy = new Proxy(
+    {},
+    {
+      get(target, prop, receiver) {
+        if (cache.has(prop)) return cache.get(prop);
 
-      const value = template[prop](proxy);
+        const value = template[prop](proxy);
 
-      cache.set(prop, value);
+        cache.set(prop, value);
 
-      return value;
-    },
-  });
+        return value;
+      },
+    }
+  );
 
   return proxy;
 };
 
-
 module.exports = {
   createReport,
-}
+};
--- index.spec.js
+++ index.spec.js
@@ -1,23 +1,23 @@
-const { createReport } = require('./index.js');
+const { createReport } = require("./index.js");
 
-describe('createReport', () => {
-  it('should error if template is not passed', () => {
+describe("createReport", () => {
+  it("should error if template is not passed", () => {
     expect(() => {
-      createReport()
+      createReport();
     }).toThrow('required argument "template" is not passed');
   });
 
-  it('should create report', () => {
+  it("should create report", () => {
     let report;
 
     expect(() => {
-      report = createReport({})
+      report = createReport({});
     }).not.toThrow();
 
     expect(report).toBeDefined();
   });
 
-  it('should calculate field', () => {
+  it("should calculate field", () => {
     const report = createReport({
       A: () => 42,
     });
@@ -25,7 +25,7 @@ describe('createReport', () => {
     expect(report.A).toEqual(42);
   });
 
-  it('should calculate field based on another', () => {
+  it("should calculate field based on another", () => {
     const report = createReport({
       A: () => 42,
       B: ({ A }) => A + 1,
@@ -34,7 +34,7 @@ describe('createReport', () => {
     expect(report.B).toEqual(42 + 1);
   });
 
-  it('should calculate field based on two fields', () => {
+  it("should calculate field based on two fields", () => {
     const report = createReport({
       A: () => 42,
       B: () => 4,
@@ -44,12 +44,12 @@ describe('createReport', () => {
     expect(report.C).toEqual(46);
   });
 
-  it('should calculate field from a chain', () => {
+  it("should calculate field from a chain", () => {
     expect(
       createReport({
         A: () => 2,
         B: ({ A }) => A + 2,
-        C: ({ B }) => B + 2
+        C: ({ B }) => B + 2,
       }).C
     ).toEqual(6);
 
@@ -59,16 +59,16 @@ describe('createReport', () => {
         B: ({ A }) => A + 1,
         C: ({ A, B }) => A + B + 1,
       }).C
-    ).toEqual(4)
+    ).toEqual(4);
   });
 
-  it('should execute field function only once', () => {
+  it("should execute field function only once", () => {
     const calculateA = jest.fn(() => 1);
 
     const report = createReport({
       A: calculateA,
     });
-      
+
     expect(calculateA.mock.calls.length).toEqual(0);
 
     report.A;
@@ -78,14 +78,14 @@ describe('createReport', () => {
     expect(calculateA.mock.calls.length).toEqual(1);
   });
 
-  it('should execute field only once for chained call', () => {
+  it("should execute field only once for chained call", () => {
     const calculateA = jest.fn(() => 1);
 
     const report = createReport({
       A: calculateA,
       B: ({ A }) => A,
     });
-      
+
     expect(calculateA.mock.calls.length).toEqual(0);
 
     report.B;
@@ -95,7 +95,7 @@ describe('createReport', () => {
     expect(calculateA.mock.calls.length).toEqual(1);
   });
 
-  it('should execute field only once for chained call', () => {
+  it("should execute field only once for chained call", () => {
     const calculateA = jest.fn(() => 1);
 
     const report = createReport({
@@ -103,7 +103,7 @@ describe('createReport', () => {
       B: ({ A }) => A,
       C: ({ B, A }) => B + A,
     });
-      
+
     expect(calculateA.mock.calls.length).toEqual(0);
 
     report.C;
